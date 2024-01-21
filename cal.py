b239230ef872af5f2c2b541ff3b624ba0af48de0
Kevin Ahr <meowmeowahr@gmail.com>
2024-01-21 13:08:22 -0600

    Update calibrator tool

--- cal.py
+++ cal.py
@@ -2,18 +2,24 @@ import cv2
 import numpy as np;
 
 # Just dummy function for callbacks from trackbar
-def nothing(x):
-    pass
+def display_hsv(x):
+    lh = cv2.getTrackbarPos("LH", "Tracking")
+    ls = cv2.getTrackbarPos("LS", "Tracking")
+    lv = cv2.getTrackbarPos("LV", "Tracking")
+    uh = cv2.getTrackbarPos("UH", "Tracking")
+    us = cv2.getTrackbarPos("US", "Tracking")
+    uv = cv2.getTrackbarPos("UV", "Tracking")
+    print((lh, ls, lv), (uh, us, uv))
 
 # Create a trackbar window to adjust the HSV values
 # They are preconfigured for a yellow object 
 cv2.namedWindow("Tracking")
-cv2.createTrackbar("LH", "Tracking", 11,255, nothing)
-cv2.createTrackbar("LS", "Tracking", 151, 255, nothing)
-cv2.createTrackbar("LV", "Tracking", 157, 255, nothing)
-cv2.createTrackbar("UH", "Tracking", 25, 255, nothing)
-cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
-cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("LH", "Tracking", 11,255, display_hsv)
+cv2.createTrackbar("LS", "Tracking", 151, 255, display_hsv)
+cv2.createTrackbar("LV", "Tracking", 157, 255, display_hsv)
+cv2.createTrackbar("UH", "Tracking", 25, 255, display_hsv)
+cv2.createTrackbar("US", "Tracking", 255, 255, display_hsv)
+cv2.createTrackbar("UV", "Tracking", 255, 255, display_hsv)
 
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
 
